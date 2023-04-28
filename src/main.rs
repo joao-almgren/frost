a9907ccc4f9d7bb2607638f7fe43941f5902ebdc
Joao Almgren <joao.almgren@ef.com>
2023-04-28 12:57:35 +0200

    Copy paste

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,233 @@
-fn main() {
-    println!("Hello, world!");
+mod wfo;
+// https://medium.com/codex/rust-modules-and-project-structure-832404a33e2e
+
+use bytemuck::{ Pod, Zeroable };
+use winit::
+{
+	window::Window,
+	window::WindowBuilder,
+	event_loop::EventLoop,
+	event::{ Event, WindowEvent },
+};
+
+#[repr(C)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+struct Vertex
+{
+	_pos: [f32; 4],
+	_col: [f32; 4],
+}
+
+fn vertex(pos: [i8; 3], col: [i8; 3]) -> Vertex
+{
+	Vertex
+	{
+		_pos: [pos[0] as f32, pos[1] as f32, pos[2] as f32, 1.0],
+		_col: [col[0] as f32, col[1] as f32, col[2] as f32, 1.0],
+	}
+}
+
+fn create_depth_texture(config: &wgpu::SurfaceConfiguration, device: &wgpu::Device,) -> wgpu::TextureView
+{
+	let depth_texture = device.create_texture(&wgpu::TextureDescriptor
+	{
+		size: wgpu::Extent3d
+		{
+			width: config.width,
+			height: config.height,
+			depth_or_array_layers: 1,
+		},
+		mip_level_count: 1,
+		sample_count: 1,
+		dimension: wgpu::TextureDimension::D2,
+		format: wgpu::TextureFormat::Depth32Float,
+		usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+		label: None,
+		view_formats: &[],
+	});
+
+	depth_texture.create_view(&wgpu::TextureViewDescriptor::default())
+}
+
+async fn run(event_loop: EventLoop<()>, window: Window)
+{
+	let size = window.inner_size();
+	let instance = wgpu::Instance::default();
+	let surface = unsafe { instance.create_surface(&window) }.unwrap();
+	let adapter = instance
+		.request_adapter(&wgpu::RequestAdapterOptions
+		{
+			power_preference: wgpu::PowerPreference::default(),
+			force_fallback_adapter: false,
+			compatible_surface: Some(&surface),
+		})
+		.await
+		.expect("Failed to find an appropriate adapter");
+
+
+	let (device, queue) = adapter
+		.request_device(
+			&wgpu::DeviceDescriptor
+			{
+				label: None,
+				features: wgpu::Features::empty(),
+				limits: wgpu::Limits::default(),
+			},
+			None,
+		)
+		.await
+		.expect("Failed to create device");
+
+
+	let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+	let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor
+	{
+		label: None,
+		bind_group_layouts: &[],
+		push_constant_ranges: &[],
+	});
+
+	let swapchain_capabilities = surface.get_capabilities(&adapter);
+	let swapchain_format = swapchain_capabilities.formats[0];
+
+	let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor
+	{
+		label: None,
+		layout: Some(&pipeline_layout),
+		vertex: wgpu::VertexState
+		{
+			module: &shader,
+			entry_point: "vs_main",
+			buffers: &[],
+		},
+		fragment: Some(wgpu::FragmentState
+		{
+			module: &shader,
+			entry_point: "fs_main",
+			targets: &[Some(swapchain_format.into())],
+		}),
+		primitive: wgpu::PrimitiveState
+		{
+			front_face: wgpu::FrontFace::Ccw,
+			cull_mode: Some(wgpu::Face::Back),
+			..Default::default()
+		},
+		depth_stencil: Some(wgpu::DepthStencilState
+		{
+			format: wgpu::TextureFormat::Depth32Float,
+			depth_write_enabled: true,
+			depth_compare: wgpu::CompareFunction::Less,
+			stencil: wgpu::StencilState::default(),
+			bias: wgpu::DepthBiasState::default(),
+		}),
+		multisample: wgpu::MultisampleState::default(),
+		multiview: None,
+	});
+
+	let mut config = wgpu::SurfaceConfiguration
+	{
+		usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+		format: swapchain_format,
+		width: size.width,
+		height: size.height,
+		present_mode: wgpu::PresentMode::Fifo,
+		alpha_mode: swapchain_capabilities.alpha_modes[0],
+		view_formats: vec![],
+	};
+
+	surface.configure(&device, &config);
+
+	let mut depth_view = create_depth_texture(&config, &device);
+
+	event_loop.run(move | event, _, control_flow |
+	{
+		let _ = (&instance, &adapter, &shader, &pipeline_layout, &config, &depth_view);
+
+		//control_flow.set_poll();
+		control_flow.set_wait();
+
+		match event
+		{
+			Event::WindowEvent { event: WindowEvent::Resized(size), .. } =>
+			{
+				config.width = size.width;
+				config.height = size.height;
+				surface.configure(&device, &config);
+				depth_view = create_depth_texture(&config, &device);
+				window.request_redraw();
+			},
+			Event::WindowEvent { event: WindowEvent::CloseRequested, .. } =>
+			{
+				control_flow.set_exit();
+			},
+			Event::MainEventsCleared =>
+			{
+				window.request_redraw();
+			},
+			Event::RedrawRequested(_) =>
+			{
+				let frame = surface
+					.get_current_texture()
+					.expect("Failed to acquire next swap chain texture");
+
+				let view = frame
+					.texture
+					.create_view(&wgpu::TextureViewDescriptor::default());
+
+				let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+				// render pass
+				{
+					let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor
+					{
+						label: None,
+						color_attachments: &[Some(wgpu::RenderPassColorAttachment
+						{
+							view: &view,
+							resolve_target: None,
+							ops: wgpu::Operations
+							{
+								load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+								store: true,
+							},
+						})],
+						depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment
+						{
+							view: &depth_view,
+							depth_ops: Some(wgpu::Operations
+							{
+								load: wgpu::LoadOp::Clear(1.0),
+								store: true,
+							}),
+							stencil_ops: None,
+						}),
+					});
+
+					rpass.set_pipeline(&render_pipeline);
+					rpass.draw(0..3, 0..1);
+				}
+
+				queue.submit(Some(encoder.finish()));
+				frame.present();
+			},
+            _ => {}
+        }
+	});
+}
+
+fn main()
+{
+	let ferris = wfo::load_wfo("rustacean-3d").unwrap();
+
+	for e in ferris
+	{
+		println!("{:?}", e);
+	}
+
+	env_logger::init();
+	let event_loop = EventLoop::new();
+	let window = WindowBuilder::new().build(&event_loop).unwrap();
+	window.set_title("hello wgpu");
+	pollster::block_on(run(event_loop, window));    
 }
---
+++ src/wfo.rs
@@ -0,0 +1,223 @@
+use std::str;
+use std::fs::File;
+use std::io::{ BufReader, BufRead };
+use std::num::IntErrorKind;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Element
+{
+	pub vertex: (f32, f32, f32),
+	pub normal: (f32, f32, f32),
+	pub color: (f32, f32, f32)
+}
+
+struct Face
+{
+	v: usize,
+	vt: usize,
+	vn: usize,
+	m: String
+}
+
+// https://en.wikipedia.org/wiki/Wavefront_.obj_file
+pub fn load_wfo(fname: &str) -> Result<Vec<Element>, Box<dyn std::error::Error>>
+{
+	let mut colors: HashMap<String, (f32, f32, f32)> = HashMap::new();
+	let mut material: String = "".to_string();
+
+	let file = File::open(format!("{}.mtl", fname))?;
+	let reader = BufReader::new(file);
+
+	for line in reader.lines()
+	{
+		let line = line?;
+
+		if line.len() > 6 && line[0..6] == *"newmtl"
+		{
+			material = line[7..].to_string();
+			colors.insert(material.clone(), (0.0, 0.0, 0.0));
+		}
+		else if line.len() > 2 && line[0..2] == *"Kd"
+		{
+			let line = line.as_bytes();
+			let index0 = find_next_space(3, line);
+			let index1 = find_next_space(index0.1, line);
+			let index2 = find_next_space(index1.1, line);
+
+			unsafe
+			{
+				*colors.get_mut(&material).unwrap() =
+				(
+					str::from_utf8_unchecked(&line[index0.0..index0.1]).parse()?,
+					str::from_utf8_unchecked(&line[index1.0..index1.1]).parse()?,
+					str::from_utf8_unchecked(&line[index2.0..index2.1]).parse()?
+				);
+			}
+		}
+	}
+
+	let mut elements: Vec<Element> = Vec::new();
+	let mut vertices: Vec<(f32, f32, f32)> = Vec::new();
+	let mut normals: Vec<(f32, f32, f32)> = Vec::new();
+	let mut material: String = "".to_string();
+
+	let file = File::open(format!("{}.obj", fname))?;
+	let reader = BufReader::new(file);
+
+	for line in reader.lines()
+	{
+		let line = line?;
+
+		if line.len() < 2
+		{
+			continue;
+		}
+
+		if line[0..2] == *"v "
+		{
+			let line = line.as_bytes();
+			let index0 = find_next_space(2, line);
+			let index1 = find_next_space(index0.1, line);
+			let index2 = find_next_space(index1.1, line);
+
+			unsafe
+			{
+				vertices.push(
+				(
+					-str::from_utf8_unchecked(&line[index0.0..index0.1]).parse()?,
+					str::from_utf8_unchecked(&line[index1.0..index1.1]).parse()?,
+					str::from_utf8_unchecked(&line[index2.0..index2.1]).parse()?
+				));
+			}
+		}
+		else if line[0..2] == *"vn"
+		{
+			let line = line.as_bytes();
+			let index0 = find_next_space(3, line);
+			let index1 = find_next_space(index0.1, line);
+			let index2 = find_next_space(index1.1, line);
+
+			unsafe
+			{
+				normals.push(
+				(
+					-str::from_utf8_unchecked(&line[index0.0..index0.1]).parse()?,
+					str::from_utf8_unchecked(&line[index1.0..index1.1]).parse()?,
+					str::from_utf8_unchecked(&line[index2.0..index2.1]).parse()?
+				));
+			}
+		}
+		else if line.len() > 6 && line[0..6] == *"usemtl"
+		{
+			material = line[7..].to_string();
+		}
+		else if line[0..2] == *"f "
+		{
+			let line = line.as_bytes();
+			let mut faces: Vec<Face> = Vec::new();
+			let mut start = 2;
+
+			loop
+			{
+				while start < line.len() && line[start] == b' '
+				{
+					start += 1;
+				}
+
+				if start == line.len()
+				{
+					break;
+				}
+
+				let index0 = find_next_slash(start, line);
+				let index1 = find_next_slash(index0.1, line);
+				let index2 = find_next_slash(index1.1, line);
+				start = index2.1;
+
+				unsafe
+				{
+					faces.push(Face
+					{
+						v: match str::from_utf8_unchecked(&line[index0.0..index0.1]).parse()
+						{
+							Ok(n) => n - 1,
+							Err::<usize, _>(error) => match error.kind()
+							{
+								IntErrorKind::Empty => 0,
+								_ => panic!("{}", error)
+							}
+						},
+						vt: match str::from_utf8_unchecked(&line[index1.0..index1.1]).parse()
+						{
+							Ok(n) => n - 1,
+							Err::<usize, _>(error) => match error.kind()
+							{
+								IntErrorKind::Empty => 0,
+								_ => panic!("{}", error)
+							}
+						},
+						vn: match str::from_utf8_unchecked(&line[index2.0..index2.1]).parse()
+						{
+							Ok(n) => n - 1,
+							Err::<usize, _>(error) => match error.kind()
+							{
+								IntErrorKind::Empty => 0,
+								_ => panic!("{}", error)
+							}
+						},
+						m: material.clone()
+					});
+				}
+			}
+
+			// triangulation (0, 2, 1) (0, 3, 2) (0, 4, 3) ...
+			if faces.len() >= 3
+			{
+				let mut start: usize = 1;
+				while start + 1 < faces.len()
+				{
+					elements.push(Element{ vertex: vertices[faces[0].v], normal: normals[faces[0].vn], color: colors[&faces[0].m] });
+					elements.push(Element{ vertex: vertices[faces[start + 1].v], normal: normals[faces[start + 1].vn], color: colors[&faces[start + 1].m] });
+					elements.push(Element{ vertex: vertices[faces[start].v], normal: normals[faces[start].vn], color: colors[&faces[start].m] });
+
+					start += 1;
+				}
+			}
+		}
+	}
+
+	Ok(elements)
+}
+
+fn find_next_space(mut i: usize, line: &[u8]) -> (usize, usize)
+{
+	while line[i] == b' '
+	{
+		i += 1;
+	}
+
+	let mut j = i;
+	while j < line.len() && line[j] != b' '
+	{
+		j += 1;
+	}
+
+	(i, j)
+}
+
+fn find_next_slash(mut i: usize, line: &[u8]) -> (usize, usize)
+{
+	if line[i] == b'/'
+	{
+		i += 1;
+	}
+
+	let mut j = i;
+	while j < line.len() && line[j] != b' ' && line[j] != b'/'
+	{
+		j += 1;
+	}
+
+	(i, j)
+}
